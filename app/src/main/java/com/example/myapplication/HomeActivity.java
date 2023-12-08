b059f922e83d95e9062c64a218b3508633c0d2f0
hwii23 <hjwright@svsu.edu>
2023-12-08 12:16:33 -0500

    Added autocomplete textview, passes "address" intent

--- app/src/main/java/com/example/myapplication/HomeActivity.java
+++ app/src/main/java/com/example/myapplication/HomeActivity.java
@@ -1,27 +1,35 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.SearchView;
 import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
 
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.Menu;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.material.navigation.NavigationView;
 
-import java.net.CookieHandler;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.net.CookieManager;
+import java.util.ArrayList;
+import java.util.List;
 
 public class HomeActivity extends AppCompatActivity {
+    final String strLocationsURL = "https://pandaexpress-rating-backend-group5.onrender.com/allPandas";
+
     SessionManager sessionManager; // SessionManager to store session
 
     // Navigation sidebar
@@ -29,8 +37,12 @@ public class HomeActivity extends AppCompatActivity {
     NavigationView navigationView;
     Toolbar toolbar;
 
+    AutoCompleteTextView atxtSearchAddress; // Search address text view
+    Button btnSearch; // Search button
+
     CookieManager cookieManager; // CookieManager to store cookies
-    SearchView svAddressSearch; // Search bar
+
+    List<String> arrPandaLocations = new ArrayList<>(); // List of Panda locations from the backend server
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,7 +58,63 @@ public class HomeActivity extends AppCompatActivity {
 
         getViews(); // Bind views
 
+        addLocations(); // Add locations to the list
+
+        // Search address text view click listener
+        searchAddress();
+
         sideNavigation(); // Navigation sidebar item click listener
+    }
+
+    public void searchAddress() {
+        // Go to list activity
+        btnSearch.setOnClickListener(v -> {
+            String strAddress = atxtSearchAddress.getText().toString(); // Get the address
+
+            // Check if the address is empty
+            if (strAddress.isEmpty())
+                Toast.makeText(this, "Please enter an address", Toast.LENGTH_SHORT).show();
+            else {
+                // Go to list activity
+//                Intent intent = new Intent(this, PandaLocationsActivity.class);
+//                intent.putExtra("address", strAddress);
+//                startActivity(intent);
+            }
+        });
+    }
+
+    // Add locations to the list
+    public void addLocations() {
+        // Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(this);
+
+        Log.d("Volley", "Requesting locations...");
+
+        // Request a string response from the provided URL.
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, strLocationsURL, null,
+            response -> {
+                try {
+                    for (int i = 0; i < response.length(); i++) {
+                        JSONObject jsonObject = response.getJSONObject(i);
+                        String strAddress = jsonObject.getString("address");
+                        arrPandaLocations.add(strAddress);
+                    }
+
+                    // Set the adapter for the search address text view
+                    ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line, arrPandaLocations);
+                    atxtSearchAddress.setAdapter(adapter);
+
+                    Log.d("Volley", "Locations requested");
+                } catch (JSONException e) {
+                    Log.d("Volley", e.toString());
+                    e.printStackTrace();
+                }
+            },
+            error -> Log.d("Volley", error.toString())
+        );
+
+        // Add the request to the RequestQueue.
+        queue.add(jsonArrayRequest);
     }
 
     // Navigation sidebar item click listener
@@ -59,8 +127,17 @@ public class HomeActivity extends AppCompatActivity {
 
             // Start the corresponding activity
             if (itemId == R.id.nav_home) {
-                // Do nothing or handle as needed
-            } else if (itemId == R.id.nav_signout) {
+                // Do nothing
+            }
+            else if (itemId == R.id.nav_locations) {
+                // Handle sign out
+//                switchActivity(new Intent(this, PandaLocationsActivity.class));
+            }
+            else if (itemId == R.id.nav_map) {
+                // Handle sign out
+//                switchActivity(new Intent(this, MapsActivity.class));
+            }
+            else if (itemId == R.id.nav_signout) {
                 // Clear the cookies
                 if (cookieManager.getCookieStore().getCookies().size() > 0)
                     cookieManager.getCookieStore().removeAll();
@@ -71,8 +148,11 @@ public class HomeActivity extends AppCompatActivity {
                 Toast.makeText(this, "Signed out successfully", Toast.LENGTH_SHORT).show();
 
                 // Handle sign out
-                startActivity(new Intent(HomeActivity.this, LoginActivity.class));
-                finish(); // Close the current activity
+                switchActivity(new Intent(this, LoginActivity.class));
+            }
+            else if (itemId == R.id.nav_settings) {
+                // Handle sign out
+//                switchActivity(new Intent(this, SettingsActivity.class));
             }
 
             // Close the navigation drawer
@@ -81,8 +161,16 @@ public class HomeActivity extends AppCompatActivity {
         });
     }
 
+    // Switch activity
+    public void switchActivity(Intent intent) {
+        startActivity(intent); // Start the activity
+        finish(); // Close the current activity
+    }
+
+    // Bind views
     public void getViews() {
-        svAddressSearch = findViewById(R.id.svAddressSearch); // Search bar
+        atxtSearchAddress = findViewById(R.id.atxtSearchAddress); // Search address text view
+        btnSearch = findViewById(R.id.btnSearch); // Search button
 
         // Navigation sidebar
         drawer = findViewById(R.id.drawer_layout);
